1632d18bf5a1079396358044c72701b614be0564
Dmitry Karavaev <dkaravaev@stc-spb.ru>
2019-06-21 16:31:52 +0300

    Migrated to python3.

--- complexnn/__init__.py
+++ complexnn/__init__.py
@@ -7,17 +7,17 @@
 # What this module includes by default:
 import bn, conv, dense, fft, init, norm, pool
 
-from   .bn    import ComplexBatchNormalization as ComplexBN
-from   .conv  import (ComplexConv,
+from   bn    import ComplexBatchNormalization as ComplexBN
+from   conv  import (ComplexConv,
                       ComplexConv1D,
                       ComplexConv2D,
                       ComplexConv3D,
                       WeightNorm_Conv)
-from   .dense import ComplexDense
-from   .fft   import fft, ifft, fft2, ifft2, FFT, IFFT, FFT2, IFFT2
-from   .init  import (ComplexIndependentFilters, IndependentFilters,
+from   dense import ComplexDense
+from   fft   import fft, ifft, fft2, ifft2, FFT, IFFT, FFT2, IFFT2
+from   init  import (ComplexIndependentFilters, IndependentFilters,
                       ComplexInit, SqrtInit)
-from   .norm  import LayerNormalization, ComplexLayerNorm
-from   .pool  import SpectralPooling1D, SpectralPooling2D
-from   .utils import (get_realpart, get_imagpart, getpart_output_shape,
+from   norm  import LayerNormalization, ComplexLayerNorm
+from   pool  import SpectralPooling1D, SpectralPooling2D
+from   utils import (get_realpart, get_imagpart, getpart_output_shape,
                       GetImag, GetReal, GetAbs)
